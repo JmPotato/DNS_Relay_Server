23f3266e055cdc4fafa4cdb8066416dd04a5b6e3
JmPotato <ghzpotato@gmail.com>
2019-02-27 16:10:25 +0800

    Support command line parameter

--- Python/dns_resolver.py
+++ Python/dns_resolver.py
@@ -5,11 +5,12 @@ class DNSResolver():
     def __init__(self, request_data):
         self.request_data = request_data
         self.result = ''
+        self.answer = b''
                 
     def parseDNSHeader(self):
         (header_id, header_flags, header_qdcount, header_ancount, header_nscount, header_arcount) = struct.unpack('>HHHHHH', self.request_data[0:12])
-        self.request_header = dict(ID=header_id, FLAGS=header_flags, QDCOUNT=header_qdcount, ANCOUNT=header_ancount, NSCOUNT=header_nscount, ARCOUNT=header_arcount)
-        return self.request_header
+        self.header = dict(ID=header_id, FLAGS=header_flags, QDCOUNT=header_qdcount, ANCOUNT=header_ancount, NSCOUNT=header_nscount, ARCOUNT=header_arcount)
+        return self.header
 
     def parseDNSQuestion(self):
         question_qname = ''
@@ -19,43 +20,60 @@ class DNSResolver():
             elif i != 0:
                 question_qname += chr(i)
         (question_qtype, question_qclass) = struct.unpack('>HH', self.request_data[-4:])
-        self.request_question = dict(QNAME=question_qname, QTYPE=question_qtype, QCLASS=question_qclass)
-        return self.request_question
+        self.question = dict(QNAME=question_qname, QTYPE=question_qtype, QCLASS=question_qclass)
+        return self.question
+
+    def parseDNSAnswer(self):
+        try:
+            (answer_qname, answer_qtype, answer_qclass, answer_ttl, answer_rdlength) = struct.unpack('>HHHLH', self.answer)
+            self.answer = dict(ANAME=answer_qname, ATYPE=answer_qtype, ACLASS=answer_qclass, ATTL=answer_ttl, ARDLENGTH=answer_rdlength, RDATA=self.result)
+        except:
+            self.answer = {}
+        return self.answer
 
     def queryLocalServer(self, local_file):
-        with open('./' + local_file, 'r') as rule_file:
-            for rule in rule_file.readlines():
-                if rule.strip().split(' ')[1] == self.request_question['QNAME']:
-                    self.result = rule.split(' ')[0]
-                    break
+        try:
+            with open('./' + local_file, 'r') as rule_file:
+                for rule in rule_file.readlines():
+                    if rule.strip().split(' ')[1] == self.question['QNAME']:
+                        self.result = rule.split(' ')[0]
+                        break
+        except:
+            self.result = ''
 
         return self.result
 
-    def queryRemoteServer(self, server_ip):
+    def queryRemoteServer(self, remote_server):
         remote_resolver = dns.resolver.Resolver()
-        remote_resolver.nameservers = [server_ip]
-        remote_respons = remote_resolver.query(self.request_question['QNAME'], 'A')
-        self.result = str(remote_respons[0])
+        remote_resolver.nameservers = [remote_server]
+        try:
+            remote_respons = remote_resolver.query(self.question['QNAME'], 'A')
+            self.result = str(remote_respons[0])
+        except:
+            self.result = ''
 
         return self.result
 
-    def queryIntegratedServer(self):
-        self.queryLocalServer('dnsrelay.txt')
-        if not self.result:
-            self.queryRemoteServer('223.5.5.5')
+    def queryIntegratedServer(self, local_file='dnsrelay.txt', remote_server='223.5.5.5'):
+        if self.question['QTYPE'] == 1:
+            self.queryLocalServer(local_file)
+            if not self.result:
+                self.queryRemoteServer(remote_server)
 
         if self.result == '' or self.result == '0.0.0.0':
             flags = 33155
             answer = 0
+            self.result == ''
         else:
             flags = 33152
             answer = 1
-        respons = struct.pack('>HHHHHH', self.request_header['ID'], flags, self.request_header['QDCOUNT'], answer, self.request_header['NSCOUNT'], self.request_header['ARCOUNT'])
-        respons += bytes(self.request_data[12:])
 
+        self.respons = struct.pack('>HHHHHH', self.header['ID'], flags, self.header['QDCOUNT'], answer, self.header['NSCOUNT'], self.header['ARCOUNT'])
+        self.respons += bytes(self.request_data[12:])
         if answer != 0:
-            respons += struct.pack('>HHHLH', 49164, 1, 1, 600, 4)
+            self.answer = struct.pack('>HHHLH', 49164, 1, 1, 600, 4)
+            self.respons += self.answer
             ip = self.result.split('.')
-            respons += struct.pack('BBBB', int(ip[0]), int(ip[1]), int(ip[2]), int(ip[3]))
+            self.respons += struct.pack('BBBB', int(ip[0]), int(ip[1]), int(ip[2]), int(ip[3]))
 
-        return respons
+        return self.respons
--- Python/server.py
+++ Python/server.py
@@ -1,9 +1,30 @@
+import sys
+import getopt
 import socketserver
 
 from dns_resolver import DNSResolver
 
 class DNSHandler(socketserver.BaseRequestHandler):
     def handle(self):
+        output_level = 0
+        remote_server = '223.5.5.5'
+        local_file = 'dnsrelay.txt'
+        try:
+            opts, args = getopt.getopt(sys.argv[1:], 'ho:f:s:', ['help', 'output=', 'filename=', 'server='])
+            for opt, arg in opts:  
+                if opt in ("-h", "--help"):
+                    print("Usage:\ndnsrelay [-d|-dd] [filename] [dns_server_upaddr]")
+                    sys.exit(1)
+                elif opt in ("-o", "--output"):
+                    output_level = int(arg)
+                elif opt in ("-f", "--filename"):
+                    local_file = arg
+                elif opt in ("-s", "--server"):
+                    remote_server = arg
+        except getopt.GetoptError:
+            print("Usage:\ndnsrelay [-d|-dd] [filename] [dns_server_upaddr]")
+            sys.exit(1)
+
         request_data = self.request[0].strip()
         request_socket = self.request[1]
 
@@ -12,13 +33,25 @@ class DNSHandler(socketserver.BaseRequestHandler):
         request_header = dns_server.parseDNSHeader()
         request_question = dns_server.parseDNSQuestion()
 
-        print(request_header)
-        print(request_question)
-
         #respons = dns_server.queryLocalServer('dnsrelay.txt')
         #respons = dns_server.queryRemoteServer('223.5.5.5')
-        respons = dns_server.queryIntegratedServer()
-        
+        respons = dns_server.queryIntegratedServer(local_file, remote_server)
+        request_answer = dns_server.parseDNSAnswer()
+
+        if output_level == 1:
+            print("QNAME: %s\tQTYPE: %s" % (request_question['QNAME'], request_question['QTYPE']))
+            if dns_server.result == '':
+                print("RESULT: %s" % "NOTFOUND")
+            else:
+                print("RESULT: %s" % dns_server.result)
+            print("====================================================================")
+        elif output_level == 2:
+            print("Header: ", request_header)
+            print("Question: ", request_question)
+            print("Answer:", request_answer)
+            print("====================================================================")
+
+
         request_socket.sendto(respons, self.client_address)
         
 if __name__ == "__main__":
