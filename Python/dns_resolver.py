9453ebd29982654463fbc00e71b7f4bedce0b7a5
JmPotato <ghzpotato@gmail.com>
2019-02-27 23:19:13 +0800

    完善注释

--- Python/dns_resolver.py
+++ Python/dns_resolver.py
@@ -2,12 +2,16 @@ import struct
 import socket
 
 class DNSResolver():
+    # 构造函数，初始化数据结构
     def __init__(self, request_data, local_file='dnsrelay.txt', remote_server='223.5.5.5'):
+        # 请求报文
         self.request = {
             'data': request_data,
             'header': self.parseDNSHeader(request_data),
             'question': self.parseDNSQuestion(request_data)
         }
+
+        # 响应报文
         response_data = self.queryIntegratedServer(local_file, remote_server)
         self.response = {
             'data': response_data,
@@ -15,7 +19,26 @@ class DNSResolver():
             'question': self.parseDNSQuestion(response_data),
             'answer': self.parseDNSAnswer(response_data)
         }
-                
+    
+    '''
+    Header format
+
+    0  1  2  3  4  5  6  7  0  1  2  3  4  5  6  7
+    +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+    |                      ID                       |
+    +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+    |QR|  opcode   |AA|TC|RD|RA|   Z    |   RCODE   |
+    +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+    |                    QDCOUNT                    |
+    +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+    |                    ANCOUNT                    |
+    +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+    |                    NSCOUNT                    |
+    +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+    |                    ARCOUNT                    |
+    +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+    '''
+    # 报文 Header 解析
     def parseDNSHeader(self, bytes_data):
         try:
             (header_id, header_flags, header_qdcount, header_ancount, header_nscount, header_arcount) = struct.unpack('>HHHHHH', bytes_data[0:12])
@@ -23,7 +46,23 @@ class DNSResolver():
         except:
             return {}
 
+    '''
+    Question format
+
+    0  1  2  3  4  5  6  7  0  1  2  3  4  5  6  7
+    +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+    |                     ...                       |
+    |                    QNAME                      |
+    |                     ...                       |
+    +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+    |                    QTYPE                      |
+    +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+    |                    QCLASS                     |
+    +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+    '''
+    # 报文 Question 解析
     def parseDNSQuestion(self, bytes_data):
+        # 还原出正常的字符串域名
         try:
             question_qname = ''
             count = 13
@@ -40,6 +79,25 @@ class DNSResolver():
         except:
             return {}
 
+    '''
+    Answer/Authority/Additional format
+
+    0  1  2  3  4  5  6  7  0  1  2  3  4  5  6  7
+    +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+    |                    NAME                       |
+    +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+    |                    TYPE                       |
+    +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+    |                    CLASS                      |
+    +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+    |                    TTL                        |
+    +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+    |                    RDLENGTH                   |
+    +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+    |                    RDATA                      |
+    +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+    '''
+    # 报文 Answer 解析
     def parseDNSAnswer(self, bytes_data):
         try:
             (answer_qtype, answer_qclass, answer_ttl, answer_rdlength, 
@@ -49,6 +107,7 @@ class DNSResolver():
         except:
             return {}
 
+    # 本地对照表查询
     def queryLocalServer(self, local_file):
         self.ip_result = ''
         if self.request['question']['QTYPE'] == 1:
@@ -61,10 +120,10 @@ class DNSResolver():
             except:
                 self.ip_result = ''
 
+        # 区分查询成功失败情况
         if self.ip_result == '' or self.ip_result == '0.0.0.0':
             flags = 33155
             answer = 0
-            self.ip_result == ''
         else:
             flags = 33152
             answer = 1
@@ -77,7 +136,9 @@ class DNSResolver():
             response_data += struct.pack('BBBB', int(ip[0]), int(ip[1]), int(ip[2]), int(ip[3]))
         return response_data
 
+    # 远程服务器中转查询
     def queryRemoteServer(self, remote_server):
+        # UDP 连接远程 DNS 服务器进行中转查询
         remote_resolver = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         remote_resolver.sendto(self.request['data'], (remote_server, 53))
         response_data = remote_resolver.recvfrom(1024)[0]
@@ -85,9 +146,10 @@ class DNSResolver():
 
         return response_data
 
+    # 综合查询函数
     def queryIntegratedServer(self, local_file, remote_server):
-        response_data = self.queryLocalServer(local_file)
+        response_data = self.queryLocalServer(local_file)           # 先进行本地查询
         if self.request['question']['QTYPE'] == 1 and self.ip_result == '':
-            response_data = self.queryRemoteServer(remote_server)
+            response_data = self.queryRemoteServer(remote_server)       # 本地未查询到记录后转发远程服务器查询
 
         return response_data
--- Python/server.py
+++ Python/server.py
@@ -5,6 +5,7 @@ import socketserver
 
 from dns_resolver import DNSResolver
 
+# 各类状态码对照表
 QTYPE = {1:'A', 2:'NS', 5:'CNAME', 6:'SOA', 12:'PTR', 15:'MX',
         16:'TXT', 17:'RP', 18:'AFSDB', 24:'SIG', 25:'KEY',
         28:'AAAA', 29:'LOC', 33:'SRV', 35:'NAPTR', 36:'KX',
@@ -20,31 +21,39 @@ RCODE = { 0:'None', 1:'Format Error', 2:'Server failure',
                  7:'YXRRSET', 8:'NXRRSET', 9:'NOTAUTH', 10:'NOTZONE'}
 OPCODE = {0:'QUERY', 1:'IQUERY', 2:'STATUS', 5:'UPDATE' }
 
+# Socket 服务器 Handler
 class DNSHandler(socketserver.BaseRequestHandler):
     def handle(self):
         output_level = 0
-        local_file = 'dnsrelay.txt'
-        remote_server = '223.5.5.5'
+        local_file = 'dnsrelay.txt'         # 默认本地查询表文件名
+        remote_server = '223.5.5.5'         # 默认远程转发服务器地址（阿里云 DNS）
         try:
             opts, args = getopt.getopt(sys.argv[1:], 'ho:f:s:', ['help', 'output=', 'filename=', 'server='])
-            for opt, arg in opts:  
+            for opt, arg in opts: 
+                # -h 或 --help 获得命令行使用帮助 
                 if opt in ("-h", "--help"):
                     print("Usage:\ndnsrelay -d [0|1|2] -f [filename] -s [dns_server_upaddr]")
                     sys.exit(1)
+                # -o 或 --output 获得输出信息，分为 0|1|2 三个等级
                 elif opt in ("-o", "--output"):
                     output_level = int(arg)
+                # -f 或 --filename 指定本地查询表文件
                 elif opt in ("-f", "--filename"):
                     local_file = arg
+                # -s 或 --server 指定远程查询服务器地址
                 elif opt in ("-s", "--server"):
                     remote_server = arg
         except getopt.GetoptError:
             print("Usage:\ndnsrelay -d [0|1|2] -f [filename] -s [dns_server_upaddr]")
             sys.exit(1)
 
-        request_data = self.request[0].strip()
-        request_socket = self.request[1]
+        request_data = self.request[0].strip()          # 接收二进制 DNS 查询报文数据
+        request_socket = self.request[1]                # 保存本次 Socket 链接信息，用于回传响应报文
 
+        # 进行 DNS 解析和查询
         dns_server = DNSResolver(request_data, local_file, remote_server)
+
+        # 分离请求报头中的状态码
         __flags__ = struct.unpack('>H', dns_server.response['data'][2:4])[0]
         flags = {
             'QR': __flags__ >> 15 & 0x0001,
@@ -56,6 +65,7 @@ class DNSHandler(socketserver.BaseRequestHandler):
             'RCODE': __flags__ & 0x000F
         }
 
+        # 在屏幕上实时打印输出信息
         if output_level == 1:
             print("QNAME: %-s\nQTYPE: %-5s %-5s\tRCODE: %s" % (dns_server.request['question']['QNAME'],
                 dns_server.request['question']['QTYPE'],
@@ -75,10 +85,10 @@ class DNSHandler(socketserver.BaseRequestHandler):
             print("Answer:", dns_server.response['answer'])
             print("====================================================================")
 
-
+        # 回传响应报文，完成 DNS 查询与中转
         request_socket.sendto(dns_server.response['data'], self.client_address)
         
 if __name__ == "__main__":
-    HOST, PORT = "localhost", 53              
-    server = socketserver.ThreadingUDPServer((HOST, PORT), DNSHandler)
+    HOST, PORT = "localhost", 53
+    server = socketserver.ThreadingUDPServer((HOST, PORT), DNSHandler)          # 启动多线程 UDP 服务器
     server.serve_forever()
