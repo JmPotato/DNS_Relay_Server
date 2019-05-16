ee1ebf44c936aa8347f79bbb1ccb0e5d060f948b
Alxead <1069469392@qq.com>
2019-05-16 20:06:11 +0800

    添加了一些注释

--- Python/server.py
+++ Python/server.py
@@ -1,26 +1,26 @@
 import sys
-import struct
-import getopt
-import socketserver
+import struct           #处理字节流的模块，用于将字节流转化为整数类型
+import getopt           #解析命令行参数的模块
+import socketserver     #
 
 from dns_resolver import DNSResolver
 
 # Socket 服务器 Handler
 class DNSHandler(socketserver.BaseRequestHandler):
     def handle(self):
-        output_level = 1
+        output_level = 1                    # 默认输出等级为1
         local_file = 'dnsrelay.txt'         # 默认本地查询表文件名
         remote_server = '223.5.5.5'         # 默认远程转发服务器地址（阿里云 DNS）
         try:
-            opts, args = getopt.getopt(sys.argv[1:], 'ho:f:s:', ['help', 'output=', 'filename=', 'server='])
-            for opt, arg in opts:
-                # -o 或 --output 获得输出信息，分为 0|1|2 三个等级
+            opts, args = getopt.getopt(sys.argv[1:], 'ho:f:s:', ['help', 'output=', 'filename=', 'server='])  #获取命令行参数，过滤掉第一个参数(脚本的文件名)
+            for opt, arg in opts:  #opts是一个两元组的列表。每个元素为：(选项串,附加参数)
+                # -o 或 --output 获得输出信息，分为 1|2 两个等级
                 if opt in ("-o", "--output"):
                     output_level = int(arg)
                 # -f 或 --filename 指定本地查询表文件
                 elif opt in ("-f", "--filename"):
                     local_file = arg
-                # -s 或 --server 指定远程查询服务器地址
+                # -s 或 --server 指定远程查询DNS服务器地址
                 elif opt in ("-s", "--server"):
                     remote_server = arg
         except getopt.GetoptError:
@@ -33,7 +33,7 @@ class DNSHandler(socketserver.BaseRequestHandler):
         # 进行 DNS 解析和查询
         dns_server = DNSResolver(request_data, local_file, remote_server)
 
-        # 在屏幕上实时打印输出信息
+        # 在屏幕上实时打印报文的信息
         if output_level == 1:
             out = "QNAME: %s\nQTYPE: %-5s %-5s\tRCODE: %s\n" % (dns_server.request['question']['QNAME'],
                 dns_server.request['question']['QTYPE'],
@@ -95,4 +95,11 @@ if __name__ == "__main__":
         sys.exit(1)
     HOST, PORT = "127.0.0.1", 53
     server = socketserver.ThreadingUDPServer((HOST, PORT), DNSHandler)          # 启动多线程 UDP 服务器
-    server.serve_forever()
+    server.serve_forever() 
+
+
+'''
+编写一个handler类,继承BaseRequestHandler,重写handle()方法,该方法处理传入的请求
+实例化一个TCPServer类或者UDPServer类，传递服务器的地址和刚才创建的请求处理类给这个server。
+调用server对象的handle_request或者serve_forever方法
+'''
